8306add5cfd84b77419aac3899331023f951206e
kizzanaome <naomekizzan@gmail.com>
2025-04-22 07:11:47 -0500

    Add lab 13 part 1 solutions

--- Lab13/src/main/java/edu/miu/lab13/prob1/Main.java
+++ Lab13/src/main/java/edu/miu/lab13/prob1/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Main {
     public static void main() {
 
-        //1st fragment
+        // a. 1st fragment
         List<Integer> ints = new ArrayList<>();
         ints.add(1);
         ints.add(2);
@@ -12,6 +12,8 @@ public class Main {
         nums.add(3.14);
 
         /** Answer:
+         *Compiler error occurs on the 4th line:
+            List<Number> nums = ints;
          * Incompatible types  cannot convert List<Integer> to List<Number>
          * because Java generics are invariant.*/
 
@@ -25,7 +27,7 @@ public class Main {
 
         /** Answer:
          * Compiler error occurs on the 5th line:
-         nums2.add(3.14);
+            nums2.add(3.14);
          Because we Cannot add to a List<? extends Number> because the exact element type is unknown.
          nums2 could be a List<Integer>, or List<Double>, or List<Float>Java doesn't know! So
          Java blocks adding to ? extends T lists to protect type safety.*/
